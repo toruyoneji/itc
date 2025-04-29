a31a3f4b91e2b63def89def7f185f53289b069ac
toruyoneji <toruyoneji0514@icloud.com>
2025-04-29 14:13:53 +0900

    ✨ detailのページ追加

--- src/nippo/urls.py
+++ src/nippo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import nippoListView
+from .views import nippoListView, nippoDetailView
+
 
 urlpatterns = [
     path("", nippoListView),
+    path("detail/", nippoDetailView),
 ]
