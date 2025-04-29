e6aaee41c74171a59e77f1584ed151e8327cb627
toruyoneji <toruyoneji0514@icloud.com>
2025-04-29 16:05:50 +0900

    ♻️ ✨ formからの値取得,urlsのname設定

--- src/nippo/urls.py
+++ src/nippo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import nippoListView, nippoDetailView
+from .views import nippoListView, nippoDetailView, nippoCreateView
 
 
 urlpatterns = [
-    path("", nippoListView),
-    path("detail/", nippoDetailView),
+    path("", nippoListView, name="nippo-list"),
+    path("detail/<int:number>", nippoDetailView, name="nippo-detail"),
+    path("create/", nippoCreateView, name="nippo-create"),
 ]
--- src/nippo/views.py
+++ src/nippo/views.py
@@ -4,11 +4,21 @@ from random import randint
 def nippoListView(request):
     return render(request, "nippo/nippo-list.html")
 
-def nippoDetailView(request):
+def nippoDetailView(request, number):
     template_name = "nippo/nippo-detail.html"
     random_int = randint(1,10)
     ctx = {
-       "random_number": random_int, 
+       "random_number": random_int,
+       "number": number, 
     }
 
     return render(request, template_name, ctx)
+
+
+def nippoCreateView(request):
+    template_name = "nippo/nippo-form.html"
+    if request.POST:
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        
+    return render(request, template_name)
